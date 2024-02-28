3295ffa39dd5996573573e3f6c4f3264f8541c78
najy97 <najy97@naver.com>
2024-02-28 15:54:20 +0900

    Update - pre/post process pipeline

---
+++ server/vital/pipeline_package/__init__.py
@@ -0,0 +1,15 @@
+from .pipeline import Pipeline
+from .detrend import Detrend
+from .bandpassfilter import BandpassFilter
+from .hilbert_normalization import HilbertNormalization
+from .smooth import Smooth
+
+preprocess_pipeline = Pipeline(steps=[
+    Smooth(),
+])
+
+postprocess_pipeline = Pipeline(steps=[
+    Detrend(),
+    BandpassFilter(filter_type="butterworth", fs=30, low=0.75, high=2.5),
+    HilbertNormalization()
+])
