ae37626c0b83551db143216c25e9b73f07453291
najy97 <najy97@naver.com>
2024-03-08 10:49:22 +0900

    Update - SpO2 calculator

--- server/vital/analysis/vital_calculator.py
+++ server/vital/analysis/vital_calculator.py
@@ -1,7 +1,7 @@
 from server.vital.analysis.visualizer import *
 import numpy as np
 import scipy
-from server.vital.pipeline_package import heartrate_pipeline, lf_hf_pipeline, detrend
+from server.vital.pipeline_package import heartrate_pipeline, lf_hf_pipeline, detrend, bandpassfilter
 
 
 class VitalCalculator:
@@ -20,6 +20,9 @@ class VitalCalculator:
         # LF/HF related
         self.ppg_lf_hf = lf_hf_pipeline.apply(self.ppg)
         self.lf_hf_ratio = 0
+
+        # SpO2 related
+        self.spo2 = 0
 
     # def visualize_ppg(self):
     #     bvp_fft_plot(self.ppg, self.fs, self.save_dict)
@@ -79,3 +82,18 @@ class VitalCalculator:
         # Calculate the LF/HF ratio
         self.lf_hf_ratio = lf_power / hf_power
         return self.lf_hf_ratio
+
+    def calc_spo2(self, RGB):
+        r_sig = bandpassfilter.BandpassFilter(filter_type="butterworth", fs=30, low=0.75, high=2.5, order=6).apply(RGB[:, 0])
+        b_sig = bandpassfilter.BandpassFilter(filter_type="butterworth", fs=30, low=0.75, high=2.5, order=6).apply(RGB[:, 2])
+
+        r_mean = np.mean(r_sig)
+        r_std = np.std(r_sig)
+
+        b_mean = np.mean(b_sig)
+        b_std = np.std(b_sig)
+
+        R = (r_std / r_mean) / (b_std / b_mean)
+
+        self.spo2 = 97.61 + 0.42 * R
+        return self.spo2
--- server/vital/pipeline_package/bandpassfilter.py
+++ server/vital/pipeline_package/bandpassfilter.py
@@ -4,21 +4,22 @@ import numpy as np
 
 
 class BandpassFilter(SignalProcessingStep):
-    def __init__(self, low, high, fs, filter_type="butterworth", **kwargs):
+    def __init__(self, low, high, fs, order=2, filter_type="butterworth", **kwargs):
         super().__init__(**kwargs)
         self.low = low
         self.high = high
         self.fs = fs
         self.filter_type = filter_type
+        self.order = order
 
     def apply(self, signal):
         if self.filter_type == "butterworth":
-            return self._butterworth_filter(signal, self.fs, self.low, self.high)
+            return self._butterworth_filter(signal)
         else:
             raise ValueError(f"Filter type '{self.filter_type}' is not supported.")
 
-    def _butterworth_filter(self, signal, fs, low, high):
-        b, a = butter(2, [low / fs * 2, high / fs * 2], btype='bandpass')
+    def _butterworth_filter(self, signal):
+        [b, a] = butter(N=self.order, Wn=[self.low / self.fs * 2, self.high / self.fs * 2], btype='bandpass')
         filtered_signal = filtfilt(b, a, signal.astype(np.double))
         return filtered_signal
 
