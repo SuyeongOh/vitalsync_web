f1bc845d26499f10701b8fb11fdc617d452bff69
SuyeongOh <pental_vega@naver.com>
2024-03-15 14:56:27 +0900

    fix login query

--- server/vital/service/__init__.py
+++ server/vital/service/__init__.py
@@ -16,7 +16,7 @@ gtSaveQuery = '''INSERT INTO GroundTruth
 
 gtLoadQuery = '''SELECT * FROM GroundTruth WHERE UserID = ?'''
 
-loginQuery = "SELECT * FROM users WHERE id = ? AND password = ?"
+loginQuery = "SELECT * FROM users WHERE user_id = ? AND password = ?"
 
 userTableQuery = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, user_id TEXT, password TEXT)"
 userRegisterQuery = "INSERT INTO users (user_id, password) VALUES (?, ?)"
