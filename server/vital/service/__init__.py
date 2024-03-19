905438968de8c1cf787247a904ee98edc2884f58
SuyeongOh <pental_vega@naver.com>
2024-03-19 11:37:00 +0900

    fix duplicated user

--- server/vital/service/__init__.py
+++ server/vital/service/__init__.py
@@ -20,3 +20,5 @@ loginQuery = "SELECT * FROM users WHERE user_id = ? AND password = ?"
 
 userTableQuery = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, user_id TEXT, password TEXT)"
 userRegisterQuery = "INSERT INTO users (user_id, password) VALUES (?, ?)"
+userCheckQuery = "SELECT COUNT(*) FROM users WHERE user_id = ?"
+
